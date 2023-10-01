6508ac1c793131d089dcebc37d316b51ba748ad0
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-01 17:55:51 +0300

    create-components

---
+++ src/api/authenticate.ts
@@ -0,0 +1,1 @@
+export type User = {  id: string;name: string;};export function authenticate(): Promise<User | undefined> {  return new Promise((resolve) =>    setTimeout(() => resolve({ id: "1", name: "Bob" }),       1000)  );}
