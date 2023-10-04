189bb66deae02cde3693be67a8a5ae70cd2ddede
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-04 20:53:46 +0300

    add-react-context

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,9 +1,16 @@
-type Props = { permissions: undefined | string[];}
+import { useAppContext } from "./AppContext";
 
-export function Content({permissions} : Props) {
-    if (permissions === undefined) {
-        return null;
-    }
+export function Content() {
+  const { permissions } = useAppContext();
+  if (permissions === undefined) {
+    return null;
+  }
 
-    return permissions.includes('admin') ? (<p className="mt-4 text-l text-center">Some important stuff that only an admin can do</p>) : (<p className="mt-4 text-l text-center">Insufficient permissions</p>) 
-} 
+  return permissions.includes("admin") ? (
+    <p className="mt-4 text-l text-center">
+      Some important stuff that only an admin can do
+    </p>
+  ) : (
+    <p className="mt-4 text-l text-center">Insufficient permissions</p>
+  );
+}
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,20 +1,16 @@
-import { User } from "./api/authenticate";
 import { Content } from "./Content";
-type Props = {
-  user: undefined | User;
-  permissions: undefined | string[];
-};
+import { useAppContext } from "./AppContext";
 
-export function Main({ user, permissions }: Props) {
+export function Main() {
+  const { user } = useAppContext();
+
   return (
     <main className="py-8">
-      <h1 className="text-3xl text-center font-bold underline">
-        Welcome
-      </h1>
+      <h1 className="text-3xl text-center font-bold underline">Welcome</h1>
       <p className="mt-8 text-xl text-center">
         {user ? `Hello ${user.name}!` : "Please sign in"}
       </p>
-      <Content permissions={permissions} />
+      <Content />
     </main>
   );
 }
